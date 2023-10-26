8dcc381a0ef51cb42b92021914bbb041e6034c63
cmkoi1 <cmkoi1003@gmail.com>
2023-10-26 18:06:36 +0900

    undo, redo, reset

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wedget/test_page.dart';
+import 'package:wedget/view/page/paint_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: TestPage(),
+      home: PaintPage(),
     );
   }
 }
