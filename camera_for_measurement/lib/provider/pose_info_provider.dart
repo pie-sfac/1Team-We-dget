9c3564a8e8aa35539daac0bfddb280ed04d8fce2
Memorylane <128967431+memorylane-dev@users.noreply.github.com>
2023-11-16 16:54:16 +0900

    sungwan

--- camera_for_measurement/lib/provider/pose_info_provider.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
-
-final poseInfoProvider = StateProvider<List<String>>((ref) => []);
--- personal_report/android/app/src/main/kotlin/com/example/personal_report/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.personal_report
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
